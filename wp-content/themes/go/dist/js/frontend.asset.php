0aeb75679f436baab506c332731fa506ffde97f0
mohamedN2018 <moh24832@gmail.com>
2025-12-23 15:32:25 +0200

    Initial WordPress project (ignore uploads)

--- wp-content/themes/go/dist/js/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '86e2a736f6884ae7cc24');
--- wp-content/themes/go/dist/js/frontend.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1a7633f4f86b7a234cbc');
